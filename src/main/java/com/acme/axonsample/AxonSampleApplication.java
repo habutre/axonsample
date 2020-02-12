be7be2ce2a833731f72c6f80eded7f0882987296
Rogerio Ramos da Silva <rogerio.ramosdasilva@takeaway.com>
2020-02-12 19:35:47 +0100

    Fix event store configuration

--- src/main/java/com/acme/axonsample/AxonSampleApplication.java
+++ src/main/java/com/acme/axonsample/AxonSampleApplication.java
@@ -1,4 +1,4 @@
-package com.acme.axonsample.axonsample;
+package com.acme.axonsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
